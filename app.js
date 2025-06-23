37f48d80f3a6e1b7c34f10761faa35794354660b
Nik <nik.palibrk@gmail.com>
2025-06-23 11:42:46 +1000

    I forgot to commit, but it doesn't  matter I will commit the current work that I've done and will start again. I will take a more simpler approach.

---
+++ app.js
@@ -0,0 +1,14 @@
+const shoes = [
+    {
+        id: "gary",
+        color: ["tan", "midnight"],
+        price: 79,
+        size: [6, 7, 8, 9, 10, 11]
+    },
+        {
+        id: "workAndWalk",
+        color: "chesnut",
+        price: 85,
+        size: [6, 7, 8, 9, 10, 11]
+    }
+];
